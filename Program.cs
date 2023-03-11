c15260387250da1a8207091a273e0f480545c8f0
Gasan <1793959@mail.rum>
2023-03-11 19:58:16 +0300

    new commit

--- Program.cs
+++ Program.cs
@@ -1,72 +1,65 @@
-﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
-
-// Генерируем массив случайных чисел 
-int[,] GenerateArray(int row, int col)
+Console.Write("Введите количество строк 1 массива: ");
+int rowsA = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов 1 массива: ");
+int columnsA = int.Parse(Console.ReadLine());
+Console.Write("Введите количество строк 2 массива: ");
+int rowsB = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов 2 массива: ");
+int columnsB = int.Parse(Console.ReadLine());
+if (columnsA != rowsB)
 {
-    int[,] array = new int[row, col]; // Создаем 2-мерный массив
-    Random random = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = random.Next(2, 10);
-        }
-    }
-    return array;
+    Console.WriteLine("Такие матрицы умножать нельзя!");
+    return;
 }
-// вводим содержимое двухмероного массива
-void PrintArray(int[,] array)
+int[,] A = GetArray(rowsA, columnsA, 1, 3);
+int[,] B = GetArray(rowsB, columnsB, 1, 3);
+PrintArray(A);
+Console.WriteLine();
+PrintArray(B);
+Console.WriteLine();
+PrintArray(GetMultiplicationMatrix(A, B));
+
+int[,] GetArray(int m, int n, int min, int max)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
     {
+        for (int j = 0; j < n; j++)
+        {
+            // result[i, j] = new Random().Next(min, max + 1);
+            result[i,j] = new Random().Next(min,max);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
         Console.WriteLine();
-        for (int j = 0; j < array.GetLength(1); j++)
+    }
+}
+
+int[,] GetMultiplicationMatrix(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
         {
-            Console.Write($"{array[i, j]}\t");
+            for (int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                arrayC[i, j] = arrayA[i, j] * arrayB[i, j];
+            }
         }
     }
-    Console.WriteLine();
-
+    return arrayC;
 }
-//метод который считает среднеарифметическое каждого клонки массива 
-double[] AverageArray(int[,] array) // заголовок метода который возвращает массив со средним значением. 
-{
-    double[]result=new double[array.GetLength(1)]; //создания массива для вычесление средного значение.
-    for (int i = 0; i < array.GetLength(1); i++) // цикл по колонкам 
-    {
-        for (int j = 0; j < array.GetLength(0); j++) // цикл по строкам 
-        {
-            result[i] += array[j, i]; // суммируем колонку
-        }
-        result[i] = result[i] / array.GetLength(0); // делим сумму на количество элементов получаем среданее значение 
-      
-    }
-    return result; 
-}
-
-void PrintAverage(double[]avg)  // Функция которая печатает массив со средними значениями. 
-{
-    for (int i = 0; i < avg.GetLength(0); i++) // цикл по элементам массива 
-    {
-        Console.Write($"{avg[i]}\t"); // печать элементов массива 
-    }
-    Console.WriteLine();
-}
-
-
-
-int[,] matrix = GenerateArray(4, 4);
-PrintArray(matrix);
-Console.WriteLine($"cреднее арифметическое элементов в каждом столбце равны = ");
-PrintAverage(AverageArray(matrix));
-
-// int[,] matrix = GenerateArray(4, 4);
-// GenerateArray(array[int,j]) ;
-// PrintArray(matrix);
-// Console.WriteLine($"индексы поискового элемента равны = {PrintArray}");
